31dc8e289a767dfd4e83c1348c11e12d32189664
BrNaka <bruno.nakayabu@gmail.com>
2021-03-12 22:52:41 -0300

    containers folder to separate modules

---
+++ containers/monetization/monetization.page.jsx
@@ -0,0 +1,14 @@
+import { getLayout as mainPageLayout } from 'components/layout/main/main.component'
+import PageLayout from 'components/layout/main/page/page.component'
+
+const Monetization = () => {
+  return (
+    <PageLayout title='Pedidos'>
+
+    </PageLayout>
+  )
+}
+
+Monetization.getLayout = mainPageLayout
+
+export default Monetization
--- pages/catalog/index.jsx
+++ pages/catalog/index.jsx
@@ -1,14 +1,3 @@
-import { getLayout as mainPageLayout } from 'components/layout/main/main.component'
-import PageLayout from 'components/layout/main/page/page.component'
+import CatalogPage from 'containers/catalog/catalog.page'
 
-const Catalog = () => {
-  return (
-    <PageLayout title='Catálogo'>
-
-    </PageLayout>
-  )
-}
-
-Catalog.getLayout = mainPageLayout
-
-export default Catalog
+export default CatalogPage
