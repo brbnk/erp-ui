097db2595f14dc2024de2356361be86113d170e4
BrNaka <bruno.nakayabu@gmail.com>
2021-04-22 22:49:18 -0300

    fix broken model

--- core/models/products.tsx
+++ core/models/products.tsx
@@ -5,13 +5,13 @@ export interface Products extends Base {
   name: string,
   discount?: number,
   price: number
-  code: string,
-  auxcode: string,
-  reference: string,
-  isactive: boolean,
-  description: string,
-  brand: string,
-  categories: string,
-  supplier: string,
-  keywords: string
+  code?: string,
+  auxcode?: string,
+  reference?: string,
+  isactive?: boolean,
+  description?: string,
+  brand?: string,
+  categories?: string,
+  supplier?: string,
+  keywords?: string
 }
